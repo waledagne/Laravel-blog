aa12cd637ea1718bf5aae45d8d6675db8b62c25c
waledagne <34514614+waledagne@users.noreply.github.com>
2021-05-07 13:22:23 -0700

    configured tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel blog</title>
+<<<<<<< HEAD
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+=======
+    <link rel = "stylesheet" href="{{asset('css/app.css')}}">
+>>>>>>> parent of 6eeb22f... configured tailwind css
 </head>
 <body class="bg-gray-100">
 
