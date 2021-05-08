f0f5f92f862fce20f4b9666674eb919dbd604c75
waledagne <34514614+waledagne@users.noreply.github.com>
2021-05-08 13:15:45 -0700

    created users

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\HTTP\Controllers\Auth\RegisterController;
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
-
+Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/posts', function () {
    return view('posts.index');
 });
