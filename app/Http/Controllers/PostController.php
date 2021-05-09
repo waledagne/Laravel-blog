ce914cfd8797d040924c27956293340a9b940a28
waledagne <34514614+waledagne@users.noreply.github.com>
2021-05-09 07:38:45 -0700

    added pagination for posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,7 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function index() {
-        $posts-> Post::get();
+        $posts = Post::paginate(2);
 
         return view('posts.index',[
             'posts' => $posts
